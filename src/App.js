a56af24a00169cf26234ebc94664c546a0914b16
yongseok kim <yongseok@saerom.co.kr>
2021-02-20 22:17:49 +0900

    add source files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Electron React App</h1>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
